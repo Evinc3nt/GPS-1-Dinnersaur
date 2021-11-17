2fa527d67f7000fb7a9cb5cfe057abfb3ad80281
Ewan-e <euanooizhanyuan@gmail.com>
2021-11-17 22:08:15 +0800

    AnimationSmootherClockChange

--- Dinnersaur GPS-1/Assets/Scripts/Euan/PlayerNoticed.cs
+++ Dinnersaur GPS-1/Assets/Scripts/Euan/PlayerNoticed.cs
@@ -13,7 +13,7 @@ public class PlayerNoticed : MonoBehaviour
     // Update is called once per frame
     IEnumerator KillSelf()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.5f);
         Destroy(this.gameObject);
     }
 }
